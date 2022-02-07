3a0c29db83b8bc5f9f6c60c7a17f7e0c778f5978
放肆 <348578429@qq.com>
2022-02-08 00:22:49 +0800

    更新java

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# fastdfs laravel java elasticsearch
-for projectname in apollo  elk  kafka  mongo mysql nacos nginxphp postgres redis rocketmq  xxljob;
+# fastdfs laravel java elasticsearch nginxphp
+# 一键启动所有容器，php,java等应用容器不会启动
+for projectname in apollo  elk  kafka  mongo mysql nacos  postgres redis rocketmq  xxljob;
 do
 (
 cd $projectname;
