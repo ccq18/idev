192bfbf017e49d5b538fe3208252eb4e42f2473c
放肆 <348578429@qq.com>
2022-02-20 13:54:09 +0800

    no message

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # fastdfs laravel java elasticsearch nginxphp
 # 一键启动所有容器，php,java等应用容器不会启动
-for projectname in apollo  elk  kafka  mongo mysql nacos  postgres redis rocketmq  xxljob;
+for projectname in  mongo mysql apollo xxljob elk nacos  kafka    postgres redis rocketmq  ;
 do
 (
 cd $projectname;
